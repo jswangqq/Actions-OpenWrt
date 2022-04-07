dea192306da506999cb2420f601ae8888575da96
jswangqq <95755304+jswangqq@users.noreply.github.com>
2022-04-07 12:15:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git op-package https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git op-small-package https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git op_app https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git op_small_app https://github.com/kenzok8/small-package' >>feeds.conf.default
